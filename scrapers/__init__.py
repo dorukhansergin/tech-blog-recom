ed7f24967e4409e4bc2f549e94e9658c5b788bda
dorukhansergin <dorukhan.sergin@gmail.com>
2025-05-09 14:51:51 -0400

    refactor: remove unused scrapers, keep only Google Research and Lyft Engineering

--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -1,7 +1,5 @@
 from .base import BaseSourceScraper
 from .google_research import GoogleResearchScraper
-from .all_things_distributed import AllThingsDistributedScraper
-from .kleppmann import KleppmannScraper
 from .lyft_engineering import LyftEngineeringScraper
 
 # Dictionary mapping source names to their respective scrapers and base URLs
@@ -10,23 +8,17 @@ from enum import Enum, auto
 
 class SourceName(str, Enum):
     GOOGLE_RESEARCH = "google_research"
-    ALL_THINGS_DISTRIBUTED = "all_things_distributed"
-    MARTIN_KLEPPMANN = "martin_kleppmann"
     LYFT_ENGINEERING = "lyft_engineering"
 
 
 SOURCE_SCRAPERS = {
     SourceName.GOOGLE_RESEARCH.value: GoogleResearchScraper,
-    SourceName.ALL_THINGS_DISTRIBUTED.value: AllThingsDistributedScraper,
-    SourceName.MARTIN_KLEPPMANN.value: KleppmannScraper,
     SourceName.LYFT_ENGINEERING.value: LyftEngineeringScraper,
 }
 
 __all__ = [
     "BaseSourceScraper",
     "GoogleResearchScraper",
-    "AllThingsDistributedScraper",
-    "KleppmannScraper",
     "LyftEngineeringScraper",
     "SOURCE_SCRAPERS",
 ]
